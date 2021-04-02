05103f90236ee28331597239ab4b9e7ca3535ade
louis03015 <louis.beaumont@gmail.com>
2021-04-02 11:02:30 +0200

    Remove unuseds

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
 import Image from '../elements/Image';
-import Modal from '../elements/Modal';
 import appStore from './../../assets/images/app-store.svg';
 import googlePlay from './../../assets/images/google-play.svg';
 import ReactTooltip from 'react-tooltip';
@@ -27,18 +24,6 @@ const Hero = ({
   invertColor,
   ...props
 }) => {
-
-  const [videoModalActive, setVideomodalactive] = useState(false);
-
-  const openModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(true);
-  }
-
-  const closeModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(false);
-  }   
 
   const outerClasses = classNames(
     'hero section center-content',
@@ -71,16 +56,6 @@ const Hero = ({
                 Augmented, better human conversations.
                 Compound relationships.
                 </p>
-              {/*<div className="reveal-from-bottom" data-reveal-delay="600">*/}
-              {/*  <ButtonGroup>*/}
-              {/*    <Button tag="a" color="primary" wideMobile href="https://cruip.com/">*/}
-              {/*      Get started*/}
-              {/*      </Button>*/}
-              {/*    <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">*/}
-              {/*      View on Github*/}
-              {/*      </Button>*/}
-              {/*  </ButtonGroup>*/}
-              {/*</div>*/}
             </div>
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
@@ -90,19 +65,6 @@ const Hero = ({
               alt="Hero"
               width={896}
               height={504} />
-            {/*<a*/}
-            {/*  data-video="https://player.vimeo.com/video/174002812"*/}
-            {/*  href="#0"*/}
-            {/*  aria-controls="video-modal"*/}
-            {/*  onClick={openModal}*/}
-            {/*>*/}
-            {/*  <Image*/}
-            {/*    className="has-shadow"*/}
-            {/*    src={require('./../../assets/images/video-placeholder.jpg')}*/}
-            {/*    alt="Hero"*/}
-            {/*    width={896}*/}
-            {/*    height={504} />*/}
-            {/*</a>*/}
           </div>
           <ReactTooltip id="main"/>
 
